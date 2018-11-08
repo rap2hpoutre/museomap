42750a8d21944df7ed3da88ae7b228e0e77d9df5
rap2h <raphaelht@gmail.com>
2018-11-08 14:31:29 +0100

    Updates

---
+++ precache-manifest.2d35ebb6219faa758a1fbb9fa4e7d48c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9500af7a274a1530a7a",
+    "url": "/museomap/static/js/runtime~main.e9500af7.js"
+  },
+  {
+    "revision": "96dc527e1c33a482c39a",
+    "url": "/museomap/static/js/main.96dc527e.chunk.js"
+  },
+  {
+    "revision": "8ae2458fadbbf73f8b56",
+    "url": "/museomap/static/js/1.8ae2458f.chunk.js"
+  },
+  {
+    "revision": "96dc527e1c33a482c39a",
+    "url": "/museomap/static/css/main.04647e8c.chunk.css"
+  },
+  {
+    "revision": "13cba838cfa5516788d198c78479d972",
+    "url": "/museomap/index.html"
+  }
+];
